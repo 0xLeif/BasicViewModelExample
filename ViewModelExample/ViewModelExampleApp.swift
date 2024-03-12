bb062fb94b2efef639d9c9cddb4e4b050e9ea9c8
Leif <zmeriksen@icloud.com>
2024-03-12 16:08:50 -0600

    Initial Commit

---
+++ ViewModelExample/ViewModelExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModelExampleApp.swift
+//  ViewModelExample
+//
+//  Created by Leif on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewModelExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
